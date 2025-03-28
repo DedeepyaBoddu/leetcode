28f47e4ebad12e93aa6f9548f369ea19df83a6b3
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2025-03-28 18:41:32 -0400

    Time: 7 ms (56.22%) | Memory: 19.3 MB (52.16%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dict={}
-        for i in nums:
-            dict[i]=dict.get(i,0)+1
-        max_freq= max(dict.values())
-        freq_key = [key for key, value in dict.items() if value == max_freq][0]
-        return freq_key
+        count = maj = 0
+        for num in nums:
+            if count==0:
+                maj=num
+            if num==maj:
+                count+=1
+            else:
+                count-=1
+        return maj
+
+            
         
