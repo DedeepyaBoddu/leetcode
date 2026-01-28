d23ebc85099a37811dfaeed2b71987b9a9401843
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-01-27 19:59:24 -0500

    Time: 4 ms (54.03%) | Memory: 19.4 MB (30.63%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        r_set = [set() for i in range(9)]
-        c_set = [set() for i in range(9)]
-        matrix = [[set() for _ in range(3)] for _ in range(3)]
+        r_set = defaultdict(set)
+        c_set = defaultdict(set)
+        matrix = defaultdict(set)
 
         for i in range(9):
             for j in range(9):
                 if board[i][j] == ".":
                     continue
-                if board[i][j] in r_set[i] or board[i][j] in c_set[j] or board[i][j] in matrix[i//3][j//3]:
+                if board[i][j] in r_set[i] or board[i][j] in c_set[j] or board[i][j] in matrix[(i//3,j//3)]:
                     return False
                 r_set[i].add(board[i][j])
                 c_set[j].add(board[i][j])
-                matrix[i//3][j//3].add(board[i][j])
+                matrix[(i//3,j//3)].add(board[i][j])
         return True
