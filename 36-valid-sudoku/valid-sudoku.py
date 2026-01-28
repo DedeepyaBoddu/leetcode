9c82554ee10ced3de3aa84de823da3188790c1c7
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-01-27 19:55:30 -0500

    Time: 1 ms (86.54%) | Memory: 19.5 MB (15.68%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,19 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set) 
-        squares = defaultdict(set)
+        r_set = [set() for i in range(9)]
+        c_set = [set() for i in range(9)]
+        matrix = [[set() for _ in range(3)] for _ in range(3)]
 
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == '.':
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
                     continue
-                if (board[r][c] in rows[r] or
-                   board[r][c] in cols[c] or
-                   board[r][c] in squares[(r // 3, c // 3)]):
-                   return False
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                if board[i][j] in r_set[i] or board[i][j] in c_set[j] or board[i][j] in matrix[i//3][j//3]:
+                    return False
+                r_set[i].add(board[i][j])
+                c_set[j].add(board[i][j])
+                matrix[i//3][j//3].add(board[i][j])
         return True
-
