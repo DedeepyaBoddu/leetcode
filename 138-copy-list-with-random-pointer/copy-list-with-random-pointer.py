77af3514998378e9b000a0253cd3bb3cba555ba3
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-10 22:20:30 -0500

    Time: 45 ms (53.69%) | Memory: 20.2 MB (12.02%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,23 +9,40 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        curr = head
+        #Interleave duplicate node after each node
+        while curr:
+            copy = Node(curr.val)
+            copy.next = curr.next
+            curr.next = copy
+            curr = copy.next
+        
+        #Assign random pointers
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+        
+        # remove interleaved nodes
+        dummy = Node(0)
+        curr_copy = dummy
+        curr = head
 
-        def DuplicateNode(node):
-            if not node:
-                return None
-            if node in hmap:
-                return hmap[node]
-            else:
-                temp = Node(node.val)
-                hmap[node] = temp
-                temp.next = DuplicateNode(node.next)
-                temp.random = DuplicateNode(node.random)
-            return temp
-
-        hmap = {}
-
-        return DuplicateNode(head)
-
-
+        while curr:
+            copy = curr.next
+            curr_copy.next = copy
+            curr.next = copy.next
+            copy.next = None
+            curr_copy = copy
+            curr = curr.next
+        
+        return dummy.next
+            
 
         
+
+
+            
