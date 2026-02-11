bfd8915d0230ede17edec18b87c4659a7db59520
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-10 23:04:54 -0500

    Time: 4 ms (48.48%) | Memory: 19.6 MB (7.59%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -11,19 +11,16 @@ class Solution:
         p1, p2 = l1, l2
         carry = 0
         while p1 or p2 or carry!=0:
-            if not p1:
-                zero_node = ListNode()
-                p1 = zero_node
-            if not p2:
-                zero_node = ListNode()
-                p2 = zero_node
-            val = (carry + p1.val + p2.val) % 10
-            carry = (carry + p1.val + p2.val) // 10
+            val1 = p1.val if p1 else 0
+            val2 = p2.val if p2 else 0
+
+            val = (carry + val1 + val2) % 10
+            carry = (carry + val1 + val2) // 10
             node = ListNode(val)
             curr.next = node
             curr = curr.next
-            p1 = p1.next
-            p2 = p2.next
+            p1 = p1.next if p1 else None
+            p2 = p2.next if p2 else None
         
         return dummy.next
 
