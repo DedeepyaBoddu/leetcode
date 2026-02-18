10540ed2a82ed3fa707f792fefbc713d2371a69e
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-18 17:35:45 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.00%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -6,9 +6,9 @@ class Solution:
             if i >= len(nums):
                 res.append(subset.copy())
                 return
-            dfs(i+1)
             subset.append(nums[i])
             dfs(i+1)
             subset.pop()
+            dfs(i+1)
         dfs(0)
         return res
