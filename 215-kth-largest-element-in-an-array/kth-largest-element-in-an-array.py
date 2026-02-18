62f8507d7b9955a4ace76d555bd9d017903274c6
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-17 20:21:15 -0500

    Time: 103 ms (18.87%) | Memory: 38.8 MB (5.03%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,36 +1,32 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
+        k = len(nums)-k
+        def quickselect(l,r):
+            pivot = nums[random.randint(l,r)]
 
-        def quickSelect(l, r):
-            pivot = nums[l]
+            left = l
+            i = l
+            right = r
 
-            # 3-way partition
-            lt = l          # nums[l:lt] < pivot
-            i = l           # current index
-            gt = r          # nums[gt:r] > pivot
-
-            while i <= gt:
-                if nums[i] < pivot:
-                    nums[lt], nums[i] = nums[i], nums[lt]
-                    lt += 1
-                    i += 1
+            while i<=right:
+                if nums[i]< pivot:
+                    nums[left], nums[i] = nums[i],nums[left]
+                    left +=1
+                    i+=1
                 elif nums[i] > pivot:
-                    nums[i], nums[gt] = nums[gt], nums[i]
-                    gt -= 1
+                    nums[right],nums[i] = nums[i], nums[right]
+                    right -=1
                 else:
-                    i += 1
-
-            # Now:
-            # nums[l:lt] < pivot
-            # nums[lt:gt+1] == pivot
-            # nums[gt+1:r] > pivot
-
-            if k < lt:
-                return quickSelect(l, lt - 1)
-            elif k > gt:
-                return quickSelect(gt + 1, r)
+                    i +=1
+            
+            if k < left:
+                return quickselect(l, left-1)
+            elif k > right:
+                return quickselect(right+1,r)
             else:
                 return nums[k]
+        return quickselect(0,len(nums)-1)
 
-        return quickSelect(0, len(nums) - 1)
+    
+
+                
