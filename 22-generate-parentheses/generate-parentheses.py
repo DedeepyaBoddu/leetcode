5ddba9069664a57632fc3543f805547f4298551e
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-19 18:23:40 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (8.07%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -4,16 +4,11 @@ class Solution:
         def dfs(string, open, close):
             if open == close == n:
                 return res.append(string)
-            elif open == close:
-                dfs(string+"(", open+1, close)
-            elif open > close:
-                if open<n:
-                    dfs(string+"(",open+1,close)
-                    dfs(string+")",open, close+1)
-                if open ==n:
-                    dfs(string+")",open, close+1)
-
-            
+            if open < n:
+                dfs(string+"(",open+1,close)
+            if close < open:
+                dfs(string+")", open, close+1)
+                       
         dfs("(",1,0)
         return res
 
