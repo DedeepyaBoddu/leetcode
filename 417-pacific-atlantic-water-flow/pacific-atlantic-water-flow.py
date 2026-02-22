8eb200813a94280086091475e0dbbe0c0dc7daab
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-22 16:06:42 -0500

    Time: 47 ms (20.88%) | Memory: 26.2 MB (5.28%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,44 +1,47 @@
-
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS = len(heights)
-        COLS = len(heights[0])
+        atl = set()
+        pac = set()
 
-        pac, atl = set(), set()
+        def pacific(i,j, prev_height):
+            if i < 0 or j < 0 or i >= len(heights) or j >= len(heights[0]) or (i,j) in pac or heights[i][j] < prev_height:
+                return
+            if i == 0 or j == 0:
+                pac.add((i,j))
+            elif heights[i][j] >= prev_height:
+                pac.add((i,j))
+            for x,y in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
+                pacific(x,y,heights[i][j])
 
-        def dfs(r, c, visited, prev_h ):
-            if (r < 0 or c < 0 or r > ROWS-1 or c > COLS-1 or (r,c) in visited or heights[r][c] < prev_h):
-                return 
-            visited.add((r,c))
+        def atlantic(i,j, prev_height):
+            if i < 0 or j < 0 or i >= len(heights) or j >= len(heights[0]) or (i,j) in atl or heights[i][j] < prev_height:
+                return
+            if i == len(heights)-1 or j == len(heights[0])-1:
+                atl.add((i,j))
+            elif heights[i][j] >= prev_height:
+                atl.add((i,j))
+            for x,y in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
+                atlantic(x,y,heights[i][j])
 
-            dfs(r-1, c, visited, heights[r][c])
-            dfs(r+1, c, visited, heights[r][c])
-            dfs(r, c-1, visited, heights[r][c])
-            dfs(r, c+1, visited, heights[r][c])
-            
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
+        for i in range(len(heights[0])):
+            pacific(0,i,0)
+            atlantic(len(heights)-1,i,0)
 
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS-1, atl, heights[r][COLS-1])
+        for j in range(len(heights)):
+            pacific(j,0,0)
+            atlantic(j,len(heights[0])-1,0)
+
 
         result = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r,c) in atl and (r,c) in pac:
-                    result.append([r,c])
+        for i in range(len(heights)):
+            for j in range(len(heights[0])):
+                if (i,j) in pac and (i,j) in atl:
+                    result.append([i,j])
         
         return result
+            
 
-        
-
-
-        
-
-        
 
 
 
-                
+        
