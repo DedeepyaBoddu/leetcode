ebe144fcc8fe93fb0cb8be0da774d6157a077f6f
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-24 16:30:22 -0500

    Time: 11 ms (24.16%) | Memory: 20.6 MB (44.32%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -2,6 +2,7 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
         parent = [i for i in range(n)]
+        rank = [1]*n
         
         def find(node):
             while parent[node]!=node:
@@ -12,7 +13,12 @@ class Solution:
             
             if p1 == p2:
                 return 0
-            parent[p2]=p1
+            if rank[p1] > rank[p2]:
+                parent[p2]=p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1]=p2
+                rank[p2] += rank[p1]
             return 1
 
         res = n
