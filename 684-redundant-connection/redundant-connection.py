51933c4cc7a0da61b99d80f010400516f6445b70
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-02-24 19:09:53 -0500

    Time: 0 ms (100.00%) | Memory: 19.9 MB (16.99%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,6 +1,7 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         parent = [i for i in range(0,len(edges)+1)]
+        rank = [1]*(len(edges)+1)
 
         def find(n):
             if parent[n]!=n:
@@ -11,7 +12,12 @@ class Solution:
             p1,p2 = find(n1), find(n2)
             if p1 == p2:
                 return False
-            parent[p2] = p1
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] +=rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
             return True
         
         for n1,n2 in edges:
