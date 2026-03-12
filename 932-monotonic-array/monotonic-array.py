2e48ea4b433bb268fafed3cbe6dd401dfc4d560d
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-03-11 21:55:32 -0400

    Time: 35 ms (62.03%) | Memory: 31.8 MB (15.86%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        inc = True
-        dec = True
+        direction = 0
         for i in range(1,len(nums)):
-            inc = inc and nums[i]>=nums[i-1]
-            if not inc:
-                break
-        for i in range(1,len(nums)):
-            dec = dec and nums[i]<=nums[i-1]
-            if not dec:
-                break
-        return inc or dec
+            diff = nums[i]-nums[i-1]
+            if diff > 0:
+                if direction < 0: return False
+                direction = 1
+            if diff < 0:
+                if direction > 0: return False
+                direction = -1
+        return True
+        
             
         
 
