10992349890fa90fb14ffc1173f4f7c04e7aad2c
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-06-19 20:34:06 -0400

    Time: 131 ms (44.96%) | Memory: 19.2 MB (90.62%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -2,15 +2,16 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
 
-        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
-
-        for j in range(len(s)):
-            dp[j][j] = True
-            count+=1
-            for i in range(j):
-                if s[i]==s[j] and ((j-i)<=2 or dp[i+1][j-1]==True):
-                    dp[i][j]= True
-                    count+=1
-        
+        def helper(l,r):
+            nonlocal count
+            while l>=0 and r< len(s) and s[l] == s[r]:
+                count +=1
+                l -=1
+                r +=1
+            
+        for i in range(len(s)):
+            helper(i,i) #odd
+            helper(i,i+1) #even
+  
         return count
         
