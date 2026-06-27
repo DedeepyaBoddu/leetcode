28cf5ffe0eda25266ce225bd80e984a03246afe6
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-06-27 12:47:18 -0400

    Time: 710 ms (53.16%) | Memory: 21.7 MB (46.44%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -3,18 +3,17 @@ class Solution:
         if sum(nums) % 2 == 1:
             return False
         target = sum(nums) // 2
-        dp = [False]*(target+1)
-        dp[0] = True
+        dp = {0}
 
         for num in nums:
-            for i in range(target,num-1,-1):
-                if dp[i]: 
-                    continue
-                if dp[i-num]:
-                    dp[i] = True
-                if dp[target]:
-                    return True
-        return False
+            temp = dp.copy()
+            for i in dp:
+                temp.add(i+num)
+                temp.add(i)
+            dp = temp
+
+
+        return True if target in dp else False
 
 
         return dp[target]
