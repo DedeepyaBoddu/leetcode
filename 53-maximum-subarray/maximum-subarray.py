511375c1fd5cdf837a0c1fb7c058dcb62c33a084
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-06-27 14:52:08 -0400

    Time: 46 ms (24.05%) | Memory: 31.3 MB (94.22%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -2,9 +2,12 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum = -float("inf")
         curr_sum = 0
-        ## Using DP - dp[i] is max sum ending at index i
+        ## Greedy - no negative prefix
         for num in nums:
-            curr_sum = max(curr_sum+num, num)
+            if curr_sum < 0:
+                curr_sum = 0
+            curr_sum += num
+            
             max_sum = max(max_sum, curr_sum)
         return max_sum
 
