fb3caa1b05ef6275e13307f8da8745529ee44ba3
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-06-29 20:56:29 -0400

    Time: 3 ms (84.39%) | Memory: 21.3 MB (78.90%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -4,11 +4,11 @@ class Solution:
             return 0
         intervals.sort()
         heap = [intervals[0][1]]
-        max_rooms = 1
+        #max_rooms = 1
         for start,end in intervals[1:]:
             if heap[0] <= start:
                 heapq.heappop(heap)
             heapq.heappush(heap,end)
-            max_rooms = max(max_rooms,len(heap))
-        return max_rooms
+            #max_rooms = max(max_rooms,len(heap))
+        return len(heap)
             
