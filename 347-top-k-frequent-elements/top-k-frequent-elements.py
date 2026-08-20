38e418431bef4e60e7802db9662ed1acbde1a7d7
Dedeepya <67430774+DedeepyaBoddu@users.noreply.github.com>
2026-08-19 21:33:05 -0400

    Time: 3051 ms (5.28%) | Memory: 22.9 MB (41.78%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
+        
         count = 0
         result = []
         for n in range(len(nums),0,-1):    
@@ -8,7 +9,7 @@ class Solution:
                 if value==n:
                     result.append(key)
                     count+=1
-                if count>=k:
+                if count==k:
                     return result
                 
         
